c55c2b0cd340ff95b3a7342ce3c53a63da24fec1
gustjd887 <gustjd887@gmail.co>
2023-03-16 16:17:47 +0900

    backend 삭제

--- terraform.tf
+++ terraform.tf
@@ -1,12 +1,12 @@
 terraform {
-  cloud {
-    hostname = "phs-tfe.kuberix.co.kr"
-    organization = "kuberix"
+#   cloud {
+#     hostname = "phs-tfe.kuberix.co.kr"
+#     organization = "kuberix"
 
-    workspaces {
-      name = "learnterraform-201"
-    }
-  }
+#     workspaces {
+#       name = "learnterraform-201"
+#     }
+#   }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
